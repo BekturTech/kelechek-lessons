a758a9cf63610e8bc1dbd58a9dc83cfdbafd62b6
Bektur Zhamalbekov <zmurik797@gmail.com>
2022-12-26 20:59:37 +0600

    variables and constants

---
+++ 14- Variables and constants/js/script.js
@@ -0,0 +1,27 @@
+let userName = "Bektur";
+console.log(userName);
+
+let userAddres = "Bishkek";
+console.log(userAddres);
+
+let num1 = 200;
+let num2 = 2021;
+document.write( "Number1 = " + num1 + "<br>", "Number2 = " + num2 + "<br>");
+
+let userNameSurname = prompt("Твои данные: ");
+let userAge = prompt("Твои возраст: ");
+let userCity = prompt("Твой город: ");
+
+document.write("<b>Твои данные: </b>" + userNameSurname + "<br>", "<b>Твои возраст: </b>" + userAge + "<br>", "<b>Твой город: </b>" + userCity + "<br>");
+
+// const pi = 3.14;
+// pi = 3.15;
+// console.log(pi);
+// будет ошибка
+
+const mainUserName = "Bektur";
+const mainUserEmail = "zmurik797@gmail.com";
+document.write(mainUserName + "<br>" + mainUserEmail + "<br>");
+
+const putUserName = prompt("Напишите свое имя, Сэр ");
+document.write("Добро пожаловать " + putUserName);
