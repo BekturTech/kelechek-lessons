6a74be5f5f828ca914d527f32c415b9c25568719
Bektur <zmurik797@gmail.com>
2024-08-31 13:19:03 +0600

    New tasks update

---
+++ 23-DOM/js/script1.js
@@ -0,0 +1,9 @@
+console.log(document.querySelector('footer a '))
+
+console.log(document.querySelectorAll('.header__menu-link'));
+
+const allSectionTags = document.querySelectorAll('section');
+
+allSectionTags.forEach(allSectionTags => {
+    console.log('Нашел section')
+});
---
+++ 23-DOM/js/script2.js
@@ -0,0 +1,13 @@
+console.log(document.querySelector('.header-logo'));
+
+const showNavLinks = document.querySelectorAll('.header-menu-item')
+
+
+showNavLinks.forEach(showNavLinks => {
+    console.log('Это ссылка')
+})
+
+
+const showLiclasses = document.querySelectorAll('li')
+
+console.log(showLiclasses)
