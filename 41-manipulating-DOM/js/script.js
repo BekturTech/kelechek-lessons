fd1b8e7d9a9edc3ef7f14bebf983b1092626aba7
Bektur <zmurik797@gmail.com>
2024-09-02 15:10:23 +0600

    update

--- 41-manipulating-DOM/js/script.js
+++ 41-manipulating-DOM/js/script.js
@@ -1,35 +1,36 @@
-window.addEventListener("load", function() {
-    const body = document.body;
-    const nav = document.createElement("nav");
-    const ul = document.createElement("ul");
+// window.addEventListener("load", function() {
+//     const body = document.body;
+//     const nav = document.createElement("nav");
+//     const ul = document.createElement("ul");
 
-    const menuList = ["home", "about", "news", "contacts"];
+//     const menuList = ["home", "about", "news", "contacts"];
 
-    // Добавляем nav в body
-    body.append(nav);
+//     // Добавляем nav в body
+//     body.append(nav);
     
-    // Добавляем ul в nav
-    nav.append(ul);
+//     // Добавляем ul в nav
+//     nav.append(ul);
 
-    // Создаем элементы списка и ссылки
-    menuList.forEach((item) => {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
+//     // Создаем элементы списка и ссылки
+//     menuList.forEach((item) => {
+//         const li = document.createElement("li");
+//         const a = document.createElement("a");
 
-        if (item === "home") {
-            a.setAttribute("href", "/");
-        } else {
-            a.setAttribute("href", `/${item}`);
-        }
+//         if (item === "home") {
+//             a.setAttribute("href", "/");
+//         } else {
+//             a.setAttribute("href", `/${item}`);
+//         }
 
-        a.textContent = item; // Устанавливаем текст ссылки
-        li.append(a); // Добавляем ссылку в элемент li
-        ul.append(li); // Добавляем элемент li в ul
-    });
-});
+//         a.textContent = item; // Устанавливаем текст ссылки
+//         li.append(a); // Добавляем ссылку в элемент li
+//         ul.append(li); // Добавляем элемент li в ul
+//     });
+// });
 
 
-window.addEventListener("load", function () {
+
+window.addEventListener("load", function() {
   const card = document.createElement("div");
   const img = document.createElement("img");
   const cardTitle = document.createElement("h2");
@@ -37,32 +38,25 @@ window.addEventListener("load", function () {
   const cardCompany = document.createElement("p");
   const socials = document.createElement("ul");
   const socialsLi = document.createElement("li");
-  const socialsLink = document.createElement("a");
-  const cardBtn = document.createElement("a");
+  const socialsLinks = document.createElement("a");
+  const cardBtn = document.createElement("button");
 
   document.body.append(card);
   card.append(img);
-  img.setAttribute(
-    "src",
-    "https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1476&q=80"
-  );
+  img.setAttribute("src",
+  "https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1476&q=80");
   img.style.width = "150px";
 
   card.append(cardTitle);
-  cardTitle.textContent = "Jon Watson";
-
+  cardTitle.textContent = "Jon watson";
   card.append(cardPosition);
   cardPosition.textContent = "CEO & Founder";
-
   card.append(cardCompany);
-  cardCompany.textContent = "Oxford";
-
+  cardCompany.textContent = "Oxford university";
   card.append(socials);
-
-  const socialItems = ["In", "Tw", "Dr", "Fb"];
-  card.append(socials);
-  socialsLi.append(socialsLink);
-
+  const socialItems = ["n", "Tw", "Dr", "Fb",]
+  socialsLi.append(socialsLinks);
+  
   socialItems.forEach((item) => {
     socials.append(socialsLi.cloneNode(true));
   });
@@ -75,5 +69,5 @@ window.addEventListener("load", function () {
 
   card.append(cardBtn);
   cardBtn.textContent = "Contact";
-  cardBtn.setAttribute("href", "hello.html");
+  cardBtn.setAttribute("hello", "window.location.href='hello.html'");
 });
