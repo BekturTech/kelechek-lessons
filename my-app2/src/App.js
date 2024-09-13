3184edf31ef00cd1307ca0ae03b5c60d01865e07
Bektur <zmurik797@gmail.com>
2024-09-13 18:09:43 +0600

    update

--- my-app2/src/App.js
+++ my-app2/src/App.js
@@ -1,9 +1,21 @@
 import UserCard from './components/UserCard/UserCard';
 import './App.scss';
 
+const users = [
+  {name: "Steve Jobs", age: 24, followers: "100k", likes: "5k", photos: "12", location: "Kyrgystan", imgUrl: "https://images.unsplash.com/photo-1562038969-e85c13ecb2ac?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+  {name: "Satoshi Nakamato", age: 23, followers: "90k", likes: "60k", photos: "95", location: "Japan", imgUrl:"https://images.unsplash.com/photo-1463453091185-61582044d556?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+  {name: "Brayan L0", age: 22, followers: "50k", likes: "15k", photos: "51", location: "USA", imgUrl: "https://plus.unsplash.com/premium_photo-1666265087928-fe19db9887ad?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+];
+
 function App() {
-  return <div classname="App">
-    <UserCard/>
+  return <div className="App">
+    
+    {
+      users.map((user, index) => (
+        <UserCard key={index} userName={user.name} userAge={user.age} userFollowers={user.followers} userLikes={user.likes} userPhotos={user.photos} userLocation={user.location} userImg={user.imgUrl}/>
+      ))
+    }
+
   </div>
 };
 
--- my-app2/src/components/UserCard/UserCard.jsx
+++ my-app2/src/components/UserCard/UserCard.jsx
@@ -1,29 +1,29 @@
 import './user-card.scss';
 import userImg from '../../assets/images/image-victor.jpg';
 
-const userCard = () => {
+const UserCard = (props) => {
     return (
         <div className="user-card">
             <div className="user-card__info">
-                <img className='user-card__img' src={userImg} alt="Victor" />
+                <img className='user-card__img' src={props.userImg} alt="Victor" />
                 <div className="user-card__user">
-                    <h3 className="user-card__name">Bektur Zhamalbekov</h3>
-                    <span className="user-card__age">18</span>
+                    <h3 className="user-card__name">{props.userName}</h3>
+                    <span className="user-card__age">{props.userAge}</span>
                 </div>
-                <p className="user-card__location">Bishkek</p>
+                <p className="user-card__location">{props.userLocation}</p>
 
                 <div className="user-card__stat">
                     <div className="user-card__stat-inner">
                         <div className="user-card__item">
-                            <p className="user-card__stat-count">100k</p>
+                            <p className="user-card__stat-count">{props.userFollowers}</p>
                             <span className="user-card__stat-descr">Followers</span>
                         </div>
                         <div className="user-card__item">
-                            <p className="user-card__stat-count">100k</p>
+                            <p className="user-card__stat-count">{props.userLikes}</p>
                             <span className="user-card__stat-descr">Likes</span>
                         </div>
                         <div className="user-card__item">
-                            <p className="user-card__stat-count">100k</p>
+                            <p className="user-card__stat-count">{props.userPhotos}</p>
                             <span className="user-card__stat-descr">Photos</span>
                         </div>
                     </div>
@@ -33,4 +33,4 @@ const userCard = () => {
     );
 };
 
-export default userCard;
+export default UserCard;
